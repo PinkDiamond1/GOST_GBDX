2527fba5d6320f6dc4f4b11f706862e7addb1a6f
bpstewar <ben.gis.stewart@gmail.com>
2018-05-18 10:01:10 -0400

    updated launching spfeas

--- launch_spfeas_gbdx.py
+++ launch_spfeas_gbdx.py
@@ -21,33 +21,33 @@ from GOST_GBDx_Tools import gbdxURL_misc
 #   For Ben, the .gbdx-config file belongs in C:\Users\WB411133 (CAUSE no one else qill f%*$&ing tell you that)
 gbdx = Interface()
 curTasks = gbdxTasks.GOSTTasks(gbdx)
-gbdxUrl = gbdxURL_misc.gbdxURL(inputParams)
+gbdxUrl = gbdxURL_misc.gbdxURL(gbdx)
 
 #Run spfeas on input imagery
-imageryFiles = r"Q:\WORKINGPROJECTS\LKA_Poverty\CharlesRocks_csv\Final_Scene_List_LKA.csv"
+imageryFiles = r"H:\SriLanka\IMAGE_CSV\Final_Scene_List_LKA.csv"
 inImages = pd.read_csv(imageryFiles)
 
 allTasks = []
-outFolder = "D:/SriLanka/SpFeasRes"
+outFolder = "H:/SriLanka/SpFeasRes"
 alreadyProcessed = os.listdir(outFolder)
 
 failedIDs = []
 orderedIDs = []
 
 for tID in inImages.iterrows():
-#    if not tID[1].ID in alreadyProcessed:
-    x = curTasks.createWorkflow(tID[1].ID, tID[1].useful_area_WKT, tID[1].Sensor, "bps/MexicoPoverty_Training/%s" % tID[1].ID,
+    if tID[1].ID in ['10300100617CED00','10300100621DA300','103001006441DA00','104001001E1F8100','1040010028022F00']:
+        x = curTasks.createWorkflow(tID[1].ID, tID[1].useful_area_WKT, tID[1].Sensor, "bps/MexicoPoverty_Training/%s" % tID[1].ID,
                     runCarFinder = 0, runSpfeas = 1, downloadImages = 1,
                     aopPan=False, aopDra=False, aopAcomp=True, aopBands='MS',
                     spfeasParams={"triggers":'orb seg ndvi dmp fourier gabor grad hog lac mean pantex saliency sfs', 
                         "scales":'8 16 32', "block":'4'})
-    try:
-        x.execute()
-    except:
-        failedIDs.append(tID[1].ID)
-        curOrder = gbdx.ordering.order(tID[1].ID)
-        orderedIDs.append(curOrder)
-        print gbdx.ordering.status(curOrder)
+        try:
+            x.execute()
+        except:
+            failedIDs.append(tID[1].ID)
+            curOrder = gbdx.ordering.order(tID[1].ID)
+            orderedIDs.append(curOrder)
+            print gbdx.ordering.status(curOrder)
 
 xx = gbdxUrl.monitorWorkflows(sleepTime=120)    
 for x in xx['FAILED']:
